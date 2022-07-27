5e178d90154584444634a4d812a0b91a1f07078f
Bella-aa <Nyashamarara28@gmail.com>
2022-07-27 01:55:23 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+question_prompts = [
+    "Which waterfall is called the smoke that thunders?\n(A) Victoria Falls\n(B) Niagra Falls\n(C) Iguazu Falls\n(D) Angel Falls\n\n",
+
+    "Which is the biggest waterfall in the world?\n(A) Iguazu Falls\n(B) Niagra Falls\n(C) Victoria Falls \n(D) Angel Falls\n\n",
+
+    "Which mountain has the tallest peak on Earth?\n(A) Mount Denali\n(B) Mount Everest\n(C)  Mount Makalu\n(D) Mount Kilimanjaro\n\n",
+
+    "Where is the Grand Canyon located?\n(A)  Peru\n(B) Norway\n(C) Ireland\n(D) Unites States\n\n",
+
+    "The largest Coral reef located in which country?\n(A) United Kingdom\n(B) Australia\n(C) France\n(D) Botswana\n\n",
+]
+
