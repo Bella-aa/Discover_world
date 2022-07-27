e6b611ca6d752c3c71b293f062b414ce94a8d6d0
Bella-aa <Nyashamarara28@gmail.com>
2022-07-27 02:32:24 +0000

    added functions

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from Question import Question
 
 question_prompts = [
     "Which waterfall is called the smoke that thunders?\n(a) Victoria Falls\n(b) Niagra Falls\n(c) Iguazu Falls\n(d) Angel Falls\n\n",
-    "Which is the biggest waterfall in the world?\n(a) Iguazu Falls\n(b) Niagra Falls\n(c) Victoria Falls \n(d) Angel Falls\n\n",
+    "Which of the following is not a waterfall?\n(a) Iguazu\n(b) Niagra\n(c) Matopos\n(d) Angel\n\n",
     "Which mountain has the tallest peak on Earth?\n(a) Mount Denali\n(b) Mount Everest\n(c)  Mount Makalu\n(d) Mount Kilimanjaro\n\n",
     "Where is the Grand Canyon located?\n(a)  Peru\n(b) Norway\n(c) Ireland\n(d) Unites States\n\n",
     "The largest Coral reef located in which country?\n(a) United Kingdom\n(b) Australia\n(c) France\n(d) Botswana\n\n",
@@ -15,3 +15,15 @@ questions = [
     Question(question_prompts[3], "d")
     Question(question_prompts[4], "b")
 ]
+
+print(questions)
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score +=1
+    print("You got" + str(score) + "/" + str(len(questions)) + "correct")
+
+run_test(questions)
